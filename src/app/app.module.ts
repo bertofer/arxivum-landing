d3ffa4b3602f38f5addf8ba3150946e89d53316c
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2018-01-15 22:46:09 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { TitleComponent } from './title/title.component';
+import { FeaturesComponent } from './features/features.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { StartedComponent } from './started/started.component';
+import { FooterComponent } from './footer/footer.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TitleComponent,
+    FeaturesComponent,
+    TechnologiesComponent,
+    StartedComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
